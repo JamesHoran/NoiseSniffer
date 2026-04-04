99401d29383be75034d667defb15d592db81cc61
James Horan <James Horan>
2026-04-04 01:38:06 -0500

    feat(app): added uplot

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,29 @@
-import './App.css'
-import { Tabs, TabsList, TabsTrigger, TabsContent } from './components/ui/tabs'
+import "./App.css";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "./components/ui/tabs";
+import React from "react";
+import uPlot from "uplot";
+import UplotReact from "uplot-react";
+import "uplot/dist/uPlot.min.css";
+
+const Chart = () => <UplotReact options={options} data={data} target={target} onCreate={chart => {}} onDelete={chart => {}} />;
 
 function App() {
   return (
     <>
       <section id="center">
         <Tabs defaultValue="tab1">
-          <TabsList>              
+          <TabsList>
             <TabsTrigger value="tab1">Tab 1</TabsTrigger>
             <TabsTrigger value="tab2">Tab 2</TabsTrigger>
-          </TabsList>                                    
-          <TabsContent value="tab1">Content 1</TabsContent>
+          </TabsList>
+          <TabsContent value="tab1">
+            <Chart />
+          </TabsContent>
           <TabsContent value="tab2">Content 2</TabsContent>
-        </Tabs> 
+        </Tabs>
       </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
