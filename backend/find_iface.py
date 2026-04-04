28f6d13b7549c0a06ac2fb284a2403d44f350eda
CrunchyToothpicks100 <wolfywoofmc@gmail.com>
2026-04-04 03:45:59 -0500

    untrack .gitignore files

--- backend/find_iface.py
+++
@@ -1,7 +0,0 @@
-from scapy.all import get_if_list, sniff
-
-for iface in get_if_list():
-    print(f"\n--- {iface} ---")
-    pkts = sniff(iface=iface, count=3, timeout=2)
-    for p in pkts:
-        print(p.summary())
